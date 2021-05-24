b5eef47f2e9cc43577640795b7b6753b5aec0898
Jonas Fehsenfeld <jonas.fehsenfeld@gmx.de>
2021-05-24 23:16:04 +0200

    Created a new View

---
+++ FinanceOverviewApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FinanceOverviewApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceOverviewApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
